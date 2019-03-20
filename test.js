2e9f52bbe8c457263ce5f73a3028c72d4e3738e0
Wayno1001 <wayno1001@gmail.com>
2019-03-20 11:17:18 +0200

    Add files via upload

--- test.js
+++ test.js
@@ -19,5 +19,13 @@ $(window).on("scroll", function() {
             $('nav').removeClass('black');
       }
 })
-
+$(document).ready(function(){   
+      setTimeout(function () {
+          $("#cookieConsent").fadeIn(200);
+       }, 4000);
+      $("#closeCookieConsent, .cookieConsentOK").click(function() {
+          $("#cookieConsent").fadeOut(200);
+      }); 
+  }); 
+  
 
